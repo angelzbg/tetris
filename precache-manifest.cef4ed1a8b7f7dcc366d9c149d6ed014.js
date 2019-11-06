eb047a926a6ed3ab0e3169873d8f1574a8d67da7
Angel Zlatanov <38349591+angelzbg@users.noreply.github.com>
2019-11-06 17:08:02 +0200

    Updates

---
+++ precache-manifest.cef4ed1a8b7f7dcc366d9c149d6ed014.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f9e08366b2e8253e1b96d3265ea8b2",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "99c4f5ff3f15bc8786ac",
+    "url": "/tetris/static/css/main.32ff1660.chunk.css"
+  },
+  {
+    "revision": "c6f8b7846964af53cf81",
+    "url": "/tetris/static/js/2.b2ddb9f2.chunk.js"
+  },
+  {
+    "revision": "99c4f5ff3f15bc8786ac",
+    "url": "/tetris/static/js/main.6f6503b1.chunk.js"
+  },
+  {
+    "revision": "78e393fdb16b5636ec78",
+    "url": "/tetris/static/js/runtime-main.4eb59c78.js"
+  }
+]);
